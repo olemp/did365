f429e1acef3fb0fb5505492c381996b0089470f6
olemp <olemp@puzzlepart.com>
2020-04-30 09:51:09 +0200

    Updates

--- status-checks/checks.js
+++ status-checks/checks.js
@@ -1,8 +1,21 @@
 // const bundleSize = require('./bundle-size');
 
+const sleep = (s) => {
+  return new Promise(resolve => {
+    setTimeout(resolve, s * 1000);
+  });
+}
+
 module.exports = [
-    {
-        name: 'Build',
-        callback: async () => 'Build success',
-      }
+  {
+    name: 'Build',
+    callback: async () => 'Build success',
+  },
+  {
+    name: 'Approved by Product Owner',
+    callback: async () => {
+      await sleep(5);
+      return 'The merge was approved';
+    }
+  }
 ];
